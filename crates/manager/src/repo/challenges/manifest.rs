d03b0c801e13e9fd11acf4e3c07b4b201af2a433
Aaron Dewes <aaron@nirvati.org>
2025-12-31 23:15:41 +0100

    Make services & volumes optional

--- crates/manager/src/repo/challenges/manifest.rs
+++ crates/manager/src/repo/challenges/manifest.rs
@@ -13,7 +13,9 @@ use crate::repo::challenges::metadata::CtfChallengeMetadata;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ChallengeYml {
+    #[serde(default)]
     pub services: HashMap<String, service::ChallengeService>,
+    #[serde(default)]
     pub volumes: HashMap<String, volume::ChallengeVolume>,
     pub metadata: CtfChallengeMetadata,
 }
